28e4aef5723c0fde2d2828d63e48453504c6d753
Goowenqiang <Goowenqiang@163.com>
2017-04-26 15:18:11 +0800

    add file hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python3
+# -*- coding:utf-8 -*-
+
+name = input()
+print("hello world!",name)
+
+print('hahah')
+
+print('辜文强  好的')
+
+
+
+s = 'Python-中文'
+print(s)
+b = s.encode('utf-8')
+print(b)
+print(b.decode('utf-8'))
